af48972934dd32d1dd6c3b4d87b28009bbca6e89
LucTotoum <eltotoum01@monccnb.ca>
2024-01-29 15:57:28 -0400

    devoir 2 solution

--- devoir2.sql
+++ devoir2.sql
@@ -1,3 +1,6 @@
+-- ci-dessous le lien git 
+-- https://github.com/LucTotoum/2095183DATA1029.git
+
 -- exercice 1
 -- À partir de la table « titles », obtenir la liste des titres contenant le mot « computer » Afficher le 
 -- titre et le prix de chaque livre.
