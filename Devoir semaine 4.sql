76fe7a4fcb83965b7bf0249623bce9d428e9bf68
LucTotoum <eltotoum01@monccnb.ca>
2024-02-13 01:30:35 -0400

    Question 3

--- Devoir semaine 4.sql
+++ Devoir semaine 4.sql
@@ -33,4 +33,21 @@ JOIN titles t ON Au.au_id = t.pub_id
 JOIN publishers P ON t.pub_id = P.pub_id
 WHERE p.pub_name = 'Eyrolles';
 
+-- Question 3-Obtenir la liste des noms d’auteurs ayant touché une avance supérieure à toutes les avances versées par l'éditeur "Harmattan".
+-- d'abord la liste des auteurs qui ont publiés chez
+-- On sélectionne le nom de l'auteur et le montant de son avance
+SELECT a.au_fname, c.avance
+FROM Authors a
+JOIN contrat c ON a.au_id = c.au_id
+-- On filtre les auteurs qui ont un contrat avec un autre éditeur que "Harmattan"
+WHERE c.pub_id <> (SELECT au_id FROM publishers WHERE pub_name = 'Harmattan')
+-- On compare l'avance de chaque auteur avec le maximum des avances versées par "Harmattan"
+AND c.avance > (SELECT MAX(avance) FROM contrat WHERE pub_id = (SELECT au_id FROM publishers WHERE pub_name = 'Harmattan'));
+use library;
+SELECT a.au_fname, c.avance
+FROM Authors a
+JOIN contrat c ON a.au_id = c.au_id
+WHERE c.pub_id <> (SELECT au_id FROM publishers WHERE pub_name = 'Harmattan')
+AND c.avance > (SELECT MAX(avance) FROM contrat WHERE pub_id = (SELECT au_id FROM publishers WHERE pub_name = 'Harmattan'));
+
 
